065a30bbf7a2da39f6e639cbe7b765118b290b71
GeethoshGv <geethoshgv.2236@gmail.com>
2024-02-06 03:31:41 +0530

    navigation style

--- client/src/Nav/Navigation.jsx
+++ client/src/Nav/Navigation.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./nav.scss";
 
 const Navigation = () => {
   return (
     <>
-      <div>
+      <div className="nav-div">
         <Link to="/login">Login</Link>
-
         <Link to="/register">Register</Link>
       </div>
     </>
