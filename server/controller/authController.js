9c6d4779816d77b51ef985f7d3e9b16d65ab4e6c
GeethoshGv <geethoshgv.2236@gmail.com>
2024-02-06 20:15:10 +0530

    Add user registration functionality with input validation in the controller file

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,7 +1,45 @@
+const User = require("../models/user.js");
+
 const test = (req, res) => {
   res.json("test is working");
 };
 
-const registerUser = (req, res) => {};
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password, conformpassword } = req.body;
+
+    //checking for name
+    if (!name) {
+      return res.json({
+        error: "name is required",
+      });
+    }
+    //check for password
+    if (!password || password.length < 6) {
+      return res.json({
+        error: "password is required and should be least 6 characters long",
+      });
+    }
+    //check for email
+
+    const exist = await User.findOne({ email });
+    if (exist) {
+      return res.json({
+        error: "email is taken already",
+      });
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+      conformpassword,
+    });
+
+    return res.json(user);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 module.exports = { test, registerUser };
