fdee07faf8eeab8196b38372a9b7f63c994cf981
GeethoshGv <geethoshgv.2236@gmail.com>
2024-02-06 20:29:08 +0530

    added react hot toast

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Navigation from "./Nav/Navigation";
+import { Toaster } from "react-hot-toast";
 
 const Layout = () => {
   return (
     <>
       <Navigation />
+      <Toaster position="bottom-right" toastOptions={{ duration: 2000 }} />
       <Outlet />
     </>
   );
